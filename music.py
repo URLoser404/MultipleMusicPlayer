93200f42232b0b8108ffa03fbabf5a54cd7ead80
URLoser404 <b0925138932@gmail.com>
2021-12-06 20:11:09 +0800

    add bug to music.py

--- music.py
+++ music.py
@@ -1,36 +1,37 @@
 import pafy
-
 import vlc
 
 url = "https://www.youtube.com/watch?v=YDxIPzijqr8"
 video = pafy.new(url)
+print(video)
 best = video.getbest()
 playurl = best.url
-Instance = vlc.Instance()
-player = Instance.media_player_new()
-Media = Instance.media_new(playurl)
-Media.get_mrl()
-player.set_media(Media)
-player.play()
 
-# import re, requests, subprocess, urllib.parse, urllib.request
-# from bs4 import BeautifulSoup
+# print(playurl)
 
-# music_name = "我站在雲林"
-# query_string = urllib.parse.urlencode({"search_query": music_name})
-# formatUrl = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+p = vlc.MediaPlayer(playurl)
+p.play()
 
-# search_results = re.findall(r"watch\?v=(\S{11})", formatUrl.read().decode())
-# clip = requests.get("https://www.youtube.com/watch?v=" + "{}".format(search_results[0]))
-# clip2 = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
+# def search():
+#     import re, requests, subprocess, urllib.parse, urllib.request
+#     from bs4 import BeautifulSoup
 
-# inspect = BeautifulSoup(clip.content, "html.parser")
-# yt_title = inspect.find_all("meta", property="og:title")
+#     music_name = "我站在雲林"
+#     query_string = urllib.parse.urlencode({"search_query": music_name})
+#     formatUrl = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
 
-# for concatMusic1 in yt_title:
-#     pass
+#     search_results = re.findall(r"watch\?v=(\S{11})", formatUrl.read().decode())
+#     clip = requests.get("https://www.youtube.com/watch?v=" + "{}".format(search_results[0]))
+#     clip2 = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
 
-# print(concatMusic1['content'])
+#     inspect = BeautifulSoup(clip.content, "html.parser")
+#     yt_title = inspect.find_all("meta", property="og:title")
+
+#     for concatMusic1 in yt_title:
+#         pass
+
+#     print(concatMusic1['content'])
+
 
 # subprocess.Popen(
 # "start /b " + "path\\to\\mpv.exe " + clip2 + " --no-video --loop=inf --input-ipc-server=\\\\.\\pipe\\mpv-pipe > output.txt",
