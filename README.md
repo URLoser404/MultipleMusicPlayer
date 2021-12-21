0230d0f147bc5124c5ceded1dbd00d67d6572a27
URLoser404 <b0925138932@gmail.com>
2021-12-21 19:12:27 +0800

    update readme

--- README.md
+++ README.md
@@ -13,6 +13,17 @@ pip install bs4
 pip install pafy
 pip install youtube-dl
 pip install python-vlc
+pip install sqlite3
+```
+
+
+Linux
+```
+./setup.sh
+```
+Windows
+```
+./setup.bat
 ```
 
 ## TODO
--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
-pip install requests;
-pip install bs4;
-pip install pafy;
-pip install youtube-dl;
-pip install python-vlc;
-pip install sqlite3;
+pip install requests
+pip install bs4
+pip install pafy
+pip install youtube-dl
+pip install python-vlc
+pip install sqlite3
